60ecab64133f1bf5d3677bd964bb49d6846b7bd9
ClutchMasterFTW <clutchmasterftw@gmail.com>
2024-08-30 06:08:37 -0400

    Plugin Initialization

---
+++ src/main/java/me/clutchmasterftw/wreckrewards/WreckRewards.java
@@ -0,0 +1,17 @@
+package me.clutchmasterftw.wreckrewards;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WreckRewards extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
